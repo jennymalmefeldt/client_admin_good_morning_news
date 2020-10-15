7795ce4ed18684b6c420040bbf83e2274e94c4c0
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-15 19:34:36 +0200

    Adding router logic to display authentication or homepage depending if the journalist is authenticated or not

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import LoginForm from './LoginForm'
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import LoginForm from "./LoginForm";
 import { Message } from "semantic-ui-react";
-import { auth } from '../modules/auth'
-
+import auth from "../modules/auth";
+import { useDispatch } from "react-redux";
 
 const Authenticate = () => {
   const [message, setMessage] = useState();
+  let history = useHistory();
+  const dispatch = useDispatch();
 
   const login = async (e) => {
     e.preventDefault();
     try {
       const email = e.target.email.value;
       const password = e.target.password.value;
-      
       const response = await auth.signIn(email, password);
-      
-      // dispatch({
-      //   type: "AUTHENTICATE",
-      //   payload: {
-      //     currentUser: {
-      //       email: "response.data.email",
-      //       role: "response.data.role"
-      //     },
-      //     authenticated: true
-      //   },
-      // });
-      props.authenticate(response.success);
-      props.setRole(response.data.role);
+
+      if (response.data.role == "journalist") {
+        dispatch({
+          type: "AUTHENTICATE",
+          payload: {
+            authenticated: response.success,
+            currentUser: response.data,
+          },
+        });
+
+        history.replace({ pathname: "/" });
+
+      } else {
+        localStorage.removeItem("J-tockAuth-Storage");
+        setMessage("Sorry, you don't have the necessary permission");
+      }
+
     } catch (error) {
+      debugger
       setMessage(error.response.data.errors[0]);
     }
   };
-
-
 
   return (
     <>
@@ -42,9 +47,8 @@ const Authenticate = () => {
           <Message.Header>{message}</Message.Header>
         </Message>
       )}
-
     </>
-  )
-}
+  );
+};
 
-export default Authenticate
+export default Authenticate;
