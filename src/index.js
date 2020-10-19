2c04487646df0263e480e482a014ce9fc3923273
emiliano-ma <emilianomainero@gmail.com>
2020-10-19 09:44:48 +0200

    Adding index.css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import "semantic-ui-css/semantic.min.css";
 import { Provider } from "react-redux";
 import configureStore from "./state/store/configureStore";
 import axios from "axios";
+import "./index.css";
+
 
 const store = configureStore();
 
